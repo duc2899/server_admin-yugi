6b81ed58705649ca9e308aeec525c4f288a9d397
duc2899 <ducne020899@gmail.com>
2026-04-05 17:18:04 +0700

    feat: Refactor authMiddleware to prioritize cookie for token retrieval and improve error handling

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,35 +1,41 @@
-import type { Response, NextFunction } from "express"
-import jwt from "jsonwebtoken"
+import type { Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import throwError from "../utils/throwError";
 import { AppRequest, JwtPayload } from "../types/common";
 import { STATUS_CODES } from "../constants/status-codes.";
 import env from "../configs/env";
 
 const authMiddleware = (req: AppRequest, res: Response, next: NextFunction) => {
-    let token: string | undefined;
-
-    // 1️⃣ lấy từ Authorization header
-    const authHeader = req.headers.authorization;
-    if (authHeader?.startsWith("Bearer ")) {
-        token = authHeader.split(" ")[1];
-    }
-
-    // 2️⃣ nếu không có thì lấy từ cookie
-    if (!token) {
-        token = req.cookies?.access_token;
-    }
-
-    if (!token) {
-        return throwError("Unauthorized", STATUS_CODES.UNAUTHORIZED);
-    }
-
     try {
-        const decoded = jwt.verify(token, env.JWT_ACCESS_SECRET as string) as JwtPayload;
+        let token: string | undefined;
+
+        // 1️⃣ Ưu tiên lấy từ cookie (web)
+        if (req.cookies?.access_token) {
+            token = req.cookies.access_token;
+        }
+
+        // 2️⃣ Nếu không có cookie thì lấy từ Authorization header (mobile)
+        if (!token) {
+            const authHeader = req.headers.authorization;
+            if (authHeader?.startsWith("Bearer ")) {
+                token = authHeader.split(" ")[1];
+            }
+        }
+
+        if (!token) {
+            throw throwError("Unauthorized", STATUS_CODES.UNAUTHORIZED);
+        }
+
+        const decoded = jwt.verify(
+            token,
+            env.JWT_ACCESS_SECRET as string
+        ) as JwtPayload;
+
         req.user = decoded;
         next();
-    } catch {
-        return throwError("Invalid or expired token", 401);
+    } catch (error) {
+        next(throwError("Invalid or expired token", STATUS_CODES.UNAUTHORIZED));
     }
 };
 
-export default authMiddleware
+export default authMiddleware;
