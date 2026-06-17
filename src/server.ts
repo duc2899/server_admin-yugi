72ce8937c070690d32ea747e7a9627b0b9576537
duc2899 <ducne020899@gmail.com>
2026-06-17 20:49:12 +0700

    refactor(socket): remove socket.io integration and related code

--- src/server.ts
+++ src/server.ts
@@ -10,19 +10,9 @@ import swaggerSpec from "./swagger";
 import { configureGracefulShutdown } from "./utils/shutdown";
 import { logger } from "./utils/logger";
 import connectRedis from "./configs/redis";
-import { Server } from 'socket.io'
-import { initSocket } from "./configs/initSocket";
-
 
 const app = express();
 const server = http.createServer(app);
-
-export const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
@@ -40,10 +30,6 @@ app.use("/api/v1", initRouter);
 
 // setup error handler
 app.use(errorHandler);
-
-// connectYugiSocket();
-
-initSocket();
 
 server.listen(env.PORT, () => {
     logger.info("Server started");
