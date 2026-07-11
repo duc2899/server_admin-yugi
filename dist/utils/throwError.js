2d648c59b0045647ca02a3abe25c22a1ae1a6a5e
duc2899 <ducne020899@gmail.com>
2026-07-12 01:23:27 +0700

    remove dist

--- dist/utils/throwError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const throwError = (message, statusCode, returnCode) => {
-    const error = new Error(message);
-    error.statusCode = statusCode;
-    error.returnCode = returnCode;
-    throw error;
-};
-exports.default = throwError;
